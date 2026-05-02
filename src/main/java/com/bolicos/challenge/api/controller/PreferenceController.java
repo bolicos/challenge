e06ed8d6caf36bc63b493391f268a668a57d89e1
bolicos <apollobolico@gmail.com>
2026-05-02 20:15:04 -0300

    feat(web): adicionar classes de controllers para welcome e para Preference.

---
+++ src/main/java/com/bolicos/challenge/api/controller/PreferenceController.java
@@ -0,0 +1,19 @@
+package com.bolicos.challenge.api.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Set;
+
+@RestController()
+@RequestMapping(path = "/api/preferences", produces = MediaType.APPLICATION_JSON_VALUE)
+public class PreferenceController {
+
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Set<String> all() {
+        return Collections.emptySet();
+    }
+}
--- src/main/java/com/bolicos/challenge/api/controller/WelcomeController.java
+++ src/main/java/com/bolicos/challenge/api/controller/WelcomeController.java
@@ -2,15 +2,18 @@ package com.bolicos.challenge.api.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class WelcomeController {
 
-    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/")
     public WelcomeResponse welcome() {
         return new WelcomeResponse("ok", "API Challenge is running");
     }
 
-    public record WelcomeResponse(String status, String message) {}
+    public record WelcomeResponse(String status, String message) {
+    }
 }
