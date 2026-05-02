170232fc87bb1e4b560995ea36308af0201eaaea
bolicos <apollobolico@gmail.com>
2026-05-02 20:17:07 -0300

    feat(persistence): adicionar entidades, relacionamento e migration de preferencias

---
+++ src/main/resources/db/migration/V2__create_preference_emails_table.sql
@@ -0,0 +1,24 @@
+CREATE TABLE emails
+(
+    id               BIGINT GENERATED ALWAYS AS IDENTITY,
+    version          BIGINT DEFAULT 0,
+    created_at      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    last_modified_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    created_by       VARCHAR(100) NOT NULL,
+    last_modified_by VARCHAR(100) NOT NULL,
+
+    preference_id    UUID         NOT NULL,
+    verified         BOOLEAN      NOT NULL,
+    email            VARCHAR(150) NOT NULL,
+    type             VARCHAR(20)  NOT NULL CHECK (
+        type IN ('PESSOAL', 'COMERCIAL')
+        ),
+
+    CONSTRAINT pk_emails PRIMARY KEY (id),
+    CONSTRAINT uk_emails_by_email UNIQUE (email),
+    CONSTRAINT fk_emails_by_preference_id FOREIGN KEY (preference_id)
+        REFERENCES communication_preferences (id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_emails_by_email ON emails (email);
+CREATE INDEX idx_emails_by_preference_id ON emails (preference_id);
