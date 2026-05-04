1584163a0565537b691f19d4e97af07996187dfd
bolicos <apollobolico@gmail.com>
2026-05-04 14:17:39 -0300

    feat(test): adicionar profile de testes em classes que sobem o contexto Spring.

--- src/test/java/com/bolicos/challenge/infrastructure/persistence/entity/CommunicationPreferenceEntityDataJpaTest.java
+++ src/test/java/com/bolicos/challenge/infrastructure/persistence/entity/CommunicationPreferenceEntityDataJpaTest.java
@@ -7,8 +7,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.UUID;
 
@@ -17,6 +19,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(JpaAuditingConfiguration.class)
 class CommunicationPreferenceEntityDataJpaTest {
 
